92d7a9c82fcb3c50f72fa10dcc902a3b5612bbbc
Gaurang <gaurangmali11@gmail.com>
2026-08-06 23:32:57 +0530

    Make Backend Directory

---
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/SmartinventoryandwarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.smartinventory.smartinventoryandwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartinventoryandwarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartinventoryandwarehouseApplication.class, args);
+	}
+
+}
