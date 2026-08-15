43ff8dd07bdd67f50eb40b6c608767bd953e9f1b
Gaurang <gaurangmali11@gmail.com>
2026-08-16 00:12:48 +0530

    Backend : Create A AuthResponseDTO File

---
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/DTO/AuthResponseDTO.java
@@ -0,0 +1,9 @@
+package com.smartinventory.smartinventoryandwarehouse.DTO;
+
+public class AuthResponseDTO {
+	
+	private String accessToken;
+	private String tokenType;
+	private String expiresIn;
+
+}
