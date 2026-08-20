ce06e8aa4a077da09f9f2f3475ed5b2afe78fcaf
Gaurang <gaurangmali11@gmail.com>
2026-08-20 23:52:49 +0530

    Make DTO Layer to for avoid data exposure. Add LoginRequestDTO file with email and password with validation constraints

---
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/DTO/LoginRequestDTO.java
@@ -0,0 +1,11 @@
+package com.smartinventory.smartinventoryandwarehouse.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(
+		@Email @NotBlank String email,
+		@NotBlank String password) {
+	
+
+}
