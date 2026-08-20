cf52d61280dcb595e88d58e5654fff2fd74a0b3d
Gaurang <gaurangmali11@gmail.com>
2026-08-21 01:37:51 +0530

    Interface add method

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/AuthService.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/AuthService.java
@@ -2,10 +2,15 @@ package com.smartinventory.smartinventoryandwarehouse.Service;
 
 
 import org.springframework.security.core.Authentication;
+
 import com.smartinventory.smartinventoryandwarehouse.DTO.LoginRequestDTO;
+import com.smartinventory.smartinventoryandwarehouse.DTO.RegisterRequestDTO;
+import com.smartinventory.smartinventoryandwarehouse.Entity.User;
 
 public interface AuthService {
 	
 	Authentication login(LoginRequestDTO request);
+
+	User register(RegisterRequestDTO request);
 
 }
