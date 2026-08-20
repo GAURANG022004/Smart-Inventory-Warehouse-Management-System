cdcf2e783b60f991c92238d6a0fa35035a7c715a
Gaurang <gaurangmali11@gmail.com>
2026-08-21 01:15:00 +0530

    Created RegisterRequestDTO

---
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/DTO/RegisterRequestDTO.java
@@ -0,0 +1,42 @@
+package com.smartinventory.smartinventoryandwarehouse.DTO;
+
+import java.time.LocalDateTime;
+
+import com.smartinventory.smartinventoryandwarehouse.Entity.User.Role;
+import com.smartinventory.smartinventoryandwarehouse.Entity.User.Status;
+
+
+public class RegisterRequestDTO {
+	
+	  long id;
+		
+		
+	   String firstName;
+	   String lastName;
+	    
+	    
+	   String email;
+	    
+	    
+	    
+	   String password;
+	    
+	    
+	   String phone;
+	    
+	    
+	    
+	   Role role;
+	    
+	    
+	    
+	    Status status;
+	    
+	    
+	    LocalDateTime createdAt;
+	    LocalDateTime updatedAt;
+	    
+
+	   
+	
+}
