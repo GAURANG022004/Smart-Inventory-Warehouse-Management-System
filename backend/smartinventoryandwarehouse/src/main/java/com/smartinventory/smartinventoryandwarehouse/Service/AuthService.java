e301153bf4d9d032154be221258e15de0534c179
Gaurang <gaurangmali11@gmail.com>
2026-08-20 23:55:22 +0530

    Interface is necessary declare login method which return Authentication

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/AuthService.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/AuthService.java
@@ -1,35 +1,11 @@
 package com.smartinventory.smartinventoryandwarehouse.Service;
 
-import java.util.Optional;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
+import com.smartinventory.smartinventoryandwarehouse.DTO.LoginRequestDTO;
 
-import com.smartinventory.smartinventoryandwarehouse.Entity.User;
-import com.smartinventory.smartinventoryandwarehouse.Repository.UserRepository;
-
-@Service
-public class AuthService {
+public interface AuthService {
 	
-	//login Request	
-	
-	public User checkUser(String email, String password) {
-		
-		Optional<User> optionalUser = userRepo.findByEmail(email);
-		
-		User user = optionalUser.get();
-		
-		if(user == null) return null;
-		
-				
-		
-		
-		
-		return user;
-		
-	}
+	Authentication login(LoginRequestDTO request);
 
 }
