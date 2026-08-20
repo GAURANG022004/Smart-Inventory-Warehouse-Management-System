498b0ea544f182900e9c744382feeacd104f9779
Gaurang <gaurangmali11@gmail.com>
2026-08-20 23:46:10 +0530

    Refine AuthCotroller with proper formating and Proper logic.

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Controller/AuthController.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Controller/AuthController.java
@@ -3,39 +3,53 @@ package com.smartinventory.smartinventoryandwarehouse.Controller;
 import java.net.http.HttpRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.smartinventory.smartinventoryandwarehouse.DTO.AuthResponseDTO;
+import com.smartinventory.smartinventoryandwarehouse.DTO.LoginRequestDTO;
+import com.smartinventory.smartinventoryandwarehouse.Entity.LoginRequest;
 import com.smartinventory.smartinventoryandwarehouse.Service.AuthService;
+import com.smartinventory.smartinventoryandwarehouse.serviceImpl.AuthServiceImpl;
 
-@Controller
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+	
+//	private HttpRequest http;
 	
 	@Autowired
 	private AuthService authservice;
 	
-	@GetMapping("/")
-	public String start() {
-		return login;
-	}
+//	@GetMapping("/")
+//	public String start() {
+//		return http.;
+//	}
+//	
+//	@GetMapping("/login")
+//	public String showLoginPage() {
+//		return login;
+//	}
 	
-	@GetMapping("/login")
-	public String showLoginPage() {
-		return login;
-	}
-	
-	@PutMapping("/login")
-	public String loginUser(@RequestParam String email, @RequestParam String password, HttpRequest request) {
+	@PostMapping("/login")
+	public ResponseEntity<Authentication> loginUser(@Valid @RequestBody LoginRequestDTO request ) {
 		
 		
-		authservice.checkUser(email, password);
+		Authentication authentication = authservice.login(request);
+		return ResponseEntity.ok(authentication);
 		
-		
-		return "AdminDashBoard";
-		return "CustomerDashBoard";
-		
+//		return "AdminDashBoard";
+//		return "CustomerDashBoard";
+//		
 	}
 	
 	
