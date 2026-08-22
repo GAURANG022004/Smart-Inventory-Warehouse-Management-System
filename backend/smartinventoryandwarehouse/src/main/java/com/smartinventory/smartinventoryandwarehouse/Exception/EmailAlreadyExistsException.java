492c9c4b92a6aaa5d29a9ff6761ba95b62c9d1fa
Gaurang <gaurangmali11@gmail.com>
2026-08-23 01:02:33 +0530

    created Email already exists exception to handle duplicate entry of email

---
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Exception/EmailAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.smartinventory.smartinventoryandwarehouse.Exception;
+
+public class EmailAlreadyExistsException extends Exception{
+	
+	
+	public EmailAlreadyExistsException(String message) {
+		
+			super(message);
+	}
+	
+	
+
+}
