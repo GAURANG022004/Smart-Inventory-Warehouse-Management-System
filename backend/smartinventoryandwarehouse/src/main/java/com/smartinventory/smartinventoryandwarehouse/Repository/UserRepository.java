3d1006d308a251f7bc2096344d20e04be427e33f
Gaurang <gaurangmali11@gmail.com>
2026-08-23 00:58:13 +0530

    Create existByEmail in UserRepository to handle exception for duplicate email entries

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Repository/UserRepository.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Repository/UserRepository.java
@@ -11,4 +11,6 @@ import com.smartinventory.smartinventoryandwarehouse.Entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	Optional<User> findByEmail(String email);
+	
+	boolean existByEmail(String email);
 }
