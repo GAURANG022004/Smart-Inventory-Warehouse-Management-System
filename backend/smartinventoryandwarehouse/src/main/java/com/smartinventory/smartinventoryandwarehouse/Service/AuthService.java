bce9fa8d690e38501b6f8f56e8cddba2b00edc70
Gaurang <gaurangmali11@gmail.com>
2026-08-23 01:03:57 +0530

    interface some method need to declare with throws cause it can lead to runtimeexception which emailexists exception

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/AuthService.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/AuthService.java
@@ -6,11 +6,12 @@ import org.springframework.security.core.Authentication;
 import com.smartinventory.smartinventoryandwarehouse.DTO.LoginRequestDTO;
 import com.smartinventory.smartinventoryandwarehouse.DTO.RegisterRequestDTO;
 import com.smartinventory.smartinventoryandwarehouse.Entity.User;
+import com.smartinventory.smartinventoryandwarehouse.Exception.EmailAlreadyExistsException;
 
 public interface AuthService {
 	
 	Authentication login(LoginRequestDTO request);
 
-	User register(RegisterRequestDTO request);
+	User register(RegisterRequestDTO request) throws EmailAlreadyExistsException;
 
 }
