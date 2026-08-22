ea9546b11dc083ef43bb69d8606762a42ca63232
Gaurang <gaurangmali11@gmail.com>
2026-08-23 01:00:02 +0530

    Added Register Request Response DTO for not returning entire users data. Explicitly it wont return sensitive hash password.

---
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Entity/RegisterResponseDTO.java
@@ -0,0 +1,15 @@
+package com.smartinventory.smartinventoryandwarehouse.Entity;
+
+public record RegisterResponseDTO (
+
+	String firstName,
+    String lastName,
+    String email,
+    String phone,
+    String role,
+    String status
+    
+    ) {
+	
+	
+}
