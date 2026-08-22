7dfdc882fb2ae72116a4a8cb97800ab35f4d9e32
Gaurang <gaurangmali11@gmail.com>
2026-08-22 23:58:56 +0530

    RegisterRequestDTO  is Done

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/DTO/RegisterRequestDTO.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/DTO/RegisterRequestDTO.java
@@ -6,7 +6,9 @@ import com.smartinventory.smartinventoryandwarehouse.Entity.User.Role;
 import com.smartinventory.smartinventoryandwarehouse.Entity.User.Status;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 
 public record RegisterRequestDTO (
@@ -14,12 +16,16 @@ public record RegisterRequestDTO (
 	 
 		
 		@Valid @NotBlank(message = "First Name is required.")
+		@Size(max = 50, message = "First name must not exceeds 50 characters")
 	    String firstName,
 	   
 	    @Valid @NotBlank(message = "Last Name is required.")
+		@Size(max = 50, message = "First name must not exceeds 50 characters")
 		String lastName,
 	    
 		@Valid @NotBlank(message = "Email is required.")
+		@Size(max = 50, message = "Email must not exceeds 50 ccharacters")
+		@Email
 		String email,
 	    
 	    
