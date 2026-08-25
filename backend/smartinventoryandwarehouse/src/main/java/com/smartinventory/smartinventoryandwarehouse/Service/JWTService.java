b91da6d50b37f97d420db95c01d8681cbf0491b5
Gaurang <gaurangmali11@gmail.com>
2026-08-25 23:59:50 +0530

    Change in interface

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/JWTService.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/JWTService.java
@@ -4,7 +4,7 @@ import com.smartinventory.smartinventoryandwarehouse.Entity.User;
 
 public interface JWTService {
 	
-	public User generatedToken(User user);
+	public String generatedToken(User user);
 	public User extractUser(User user);
 	public User extractEmail(User user);
 	public User extractRole(token);
