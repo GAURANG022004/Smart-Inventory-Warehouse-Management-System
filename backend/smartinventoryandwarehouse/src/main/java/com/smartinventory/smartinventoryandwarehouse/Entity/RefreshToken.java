41b1f28590e2c3ac0e2afd9855d3eb7dd01f8a22
Gaurang <gaurangmali11@gmail.com>
2026-08-27 23:31:33 +0530

    Create Entity RefreshToken

---
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Entity/RefreshToken.java
@@ -0,0 +1,118 @@
+package com.smartinventory.smartinventoryandwarehouse.Entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(
+		name = "refresh_tokens",
+		indexes = {
+				@Index(name = "idx_refresh_token_hash", columnList = "token_hash"),
+				@Index(name = "idx_refresh_token_user", columnList = "user_id")
+		}
+	)
+public class RefreshToken {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(
+			name = "user_id",
+			nullable = false
+			)
+	
+	private User user;
+	
+	
+	@Column(
+			name = "token_hash",
+			nullable = false,
+			unique = true,
+			length = 64
+			)
+	private String tokenHash;
+	
+	 @Column(
+		        name = "expires_at",
+		        nullable = false
+		    )
+    private Instant expiresAt;
+
+    @Column(
+        name = "created_at",
+        nullable = false
+    )
+    private Instant createdAt;
+
+    @Column(
+        name = "revoked_at"
+    )
+    private Instant revokedAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getTokenHash() {
+		return tokenHash;
+	}
+
+	public void setTokenHash(String tokenHash) {
+		this.tokenHash = tokenHash;
+	}
+
+	public Instant getExpiresAt() {
+		return expiresAt;
+	}
+
+	public void setExpiresAt(Instant expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Instant getRevokedAt() {
+		return revokedAt;
+	}
+
+	public void setRevokedAt(Instant revokedAt) {
+		this.revokedAt = revokedAt;
+	}
+    
+    
+    
+    
+	
+	
+	
+
+}
