04b24686cca1a7b88cfc6b7f334fc352f21d99e5
Gaurang <gaurangmali11@gmail.com>
2026-08-27 23:33:21 +0530

    JWT Service add methods like generateToken and isTokenExpired method

--- backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/JWTService.java
+++ backend/smartinventoryandwarehouse/src/main/java/com/smartinventory/smartinventoryandwarehouse/Service/JWTService.java
@@ -4,11 +4,11 @@ import com.smartinventory.smartinventoryandwarehouse.Entity.User;
 
 public interface JWTService {
 	
-	public String generatedToken(User user);
-	public User extractUser(User user);
-	public User extractEmail(User user);
-	public User extractRole(token);
-	public User isTokenExpired(token);
-	public validateToken(token, user);
+	public String generateToken(User user);
+	public long extractUserId(String token);
+	public String extractEmail(String token);
+	public String extractRole(String token);
+	public boolean isTokenExpired(String token);
+	public boolean validateToken(String token, User user);
 
 }
